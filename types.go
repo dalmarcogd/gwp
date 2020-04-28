b0b4f28cd3dd6aa515745accfa1a22ef1fe14149
Guilherme Dalmarco <dalmarco.gd@gmail.com>
2020-04-28 11:50:29 -0300

    refactore fodler

--- types.go
+++
@@ -1,6 +0,0 @@
-package go_worker_pool
-
-import (
-	"github.com/dalmarcogd/go-worker-pool/worker"
-)
-
--- worker/worker_test.go
+++ worker/worker_test.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"errors"
 	"fmt"
+	"log"
 	"testing"
 	"time"
 )
@@ -141,5 +142,68 @@ func TestWorker_IsUp(t *testing.T) {
 	<-time.After(3 * time.Second)
 	if w.IsUp() {
 		t.Errorf("Was expect that worker is Down, but returned: Up")
+	}
+}
+
+func TestRunWorkers(t *testing.T) {
+	handleErrors := func(w *Worker, err error) {
+		log.Print(err)
+	}
+
+	workers := []*Worker{
+		NewWorker("w1",
+			func() error {
+				<-time.After(1 * time.Second)
+				return nil
+			},
+			1,
+			true),
+	}
+
+	go func() {
+		if err := RunWorkers(workers, handleErrors); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	<-time.After(2 * time.Second)
+	for _, worker := range workers {
+		if worker.Restarts <= 0 {
+			t.Error("Worker setup to restart always but not restarted")
+		}
+	}
+}
+
+func Test_runWorkerHandleError(t *testing.T) {
+	hasError := false
+	handleErrors := func(w *Worker, err error) {
+		log.Print(err)
+		hasError = true
+	}
+
+	workers := []*Worker{
+		NewWorker("w1",
+			func() error {
+				<-time.After(1 * time.Second)
+				return errors.New("happened some error")
+			},
+			1,
+			false),
+	}
+
+	go func() {
+		if err := RunWorkers(workers, handleErrors); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	<-time.After(3 * time.Second)
+	for _, worker := range workers {
+		if worker.Status()["w1-1"] != ERROR {
+			t.Error("Worker setup to return error but not returned")
+		}
+	}
+	if !hasError {
+		t.Error("Expected error handled")
 	}
 }
