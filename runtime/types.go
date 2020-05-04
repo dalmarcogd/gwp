555564bfb878217db02592fbba6fae7113c4b73a
Guilherme Dalmarco <dalmarco.gd@gmail.com>
2020-05-04 14:07:10 -0300

    #issue30 - enhancement to allow custom health check

--- runtime/types.go
+++ runtime/types.go
@@ -9,10 +9,6 @@ type (
 		Healthy() bool
 	}
 
-	CheckHealthy interface {
-		Healthy() bool
-	}
-
 	//FakeServer interface that define the contract to be used between monitor.http and workerServer
 	//for tests only
 	FakeServer struct{}
